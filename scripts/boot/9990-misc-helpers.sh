2908ddf3d6157b996b516f9e28b2fba49846bd99
Daniel Baumann <daniel@debian.org>
2013-02-19 05:48:32 +0100

    Making loadkeys call quiet in order to not mess-up persistence, thanks to stack <stack@inventati.org> (Closes: #700902).

--- scripts/boot/9990-misc-helpers.sh
+++ scripts/boot/9990-misc-helpers.sh
@@ -608,7 +608,7 @@ load_keymap ()
 	# Load custom keymap
 	if [ -x /bin/loadkeys -a -r /etc/boottime.kmap.gz ]
 	then
-		loadkeys /etc/boottime.kmap.gz
+		loadkeys --quiet /etc/boottime.kmap.gz
 	fi
 }
 
