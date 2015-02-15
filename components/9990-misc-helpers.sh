289f0d924b6fa009790b4a67164892107a5a4086
Tails developers <amnesia@boum.org>
2015-02-15 17:26:47 +0000

    Removing attempt to mount overlayfs in an obsolete way.
    The version of overlayfs that reached Linux mainline does not support
    this mount call, no need to try it every time.

--- components/9990-misc-helpers.sh
+++ components/9990-misc-helpers.sh
@@ -1318,19 +1318,16 @@ do_union ()
 				panic "overlay needs at least one lower filesystem (read-only branch)."
 			fi
 			unionmountopts="-o noatime,lowerdir=${unionro},upperdir=${unionrw}"
-			if ! mount -t ${UNIONTYPE} ${unionmountopts} ${UNIONTYPE} "${unionmountpoint}" 2>/dev/null
-			then
-				# Ref: kiwi from OpenSuse kiwi-7.02.18-1.1
-				# overlayfs in version >= v22 behaves differently
-				# + renamed from overlayfs to overlay
-				# + requires a workdir to become mounted
-				# + requires workdir and upperdir to reside under the same mount
-				# + requires workdir and upperdir to be in separate subdirs
-				mkdir ${unionrw}/rw
-				mkdir ${unionrw}/work
-				unionmountopts="-o noatime,lowerdir=${unionro},upperdir=${unionrw}/rw,workdir=${unionrw}/work"
-				mount -t ${UNIONTYPE} ${unionmountopts} ${UNIONTYPE} "${unionmountpoint}"
-			fi
+			# Ref: kiwi from OpenSuse kiwi-7.02.18-1.1
+			# overlayfs in version >= v22 behaves differently
+			# + renamed from overlayfs to overlay
+			# + requires a workdir to become mounted
+			# + requires workdir and upperdir to reside under the same mount
+			# + requires workdir and upperdir to be in separate subdirs
+			mkdir ${unionrw}/rw
+			mkdir ${unionrw}/work
+			unionmountopts="-o noatime,lowerdir=${unionro},upperdir=${unionrw}/rw,workdir=${unionrw}/work"
+			mount -t ${UNIONTYPE} ${unionmountopts} ${UNIONTYPE} "${unionmountpoint}"
 			;;
 	esac
 }
