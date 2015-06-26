80b9733997a5f1c17a61f2a98c06689bd40e312e
Michael Prokop <mika@grml.org>
2015-06-26 14:43:38 +0200

    update boot failure contact instructions

--- components/9990-initramfs-tools.sh
+++ components/9990-initramfs-tools.sh
@@ -52,14 +52,20 @@ panic()
 	printf "\n\n"
 	printf "  \033[1;37mBOOT FAILED!\033[0m\n"
 	printf "\n"
-	printf "  This Live System image failed to boot.\n\n"
+	printf "  This image failed to boot.\n\n"
 
-	printf "  Please file a bug against the 'live-boot' package or email the Live Systems\n"
-	printf "  mailing list at <debian-live@lists.debian.org>, making sure to note the\n"
-	printf "  exact version, name and distribution of the image you were attempting to boot.\n\n"
+	printf "  Please file a bug at your distributors bug tracking system, making\n"
+	printf "  sure to note the exact version, name and distribution of the image\n"
+	printf "  you were attempting to boot.\n\n"
+
+	if [ -r /etc/grml_version ]
+	then
+		GRML_VERSION="$(cat /etc/grml_version)"
+		printf "  $GRML_VERSION\n\n"
+	fi
 
 	printf "  The file ${LIVELOG} contains some debugging information but booting with the\n"
-	printf "  ${DEBUG} command-line parameter will greatly increase its verbosity which is\n"
+	printf "  ${DEBUG}=1 command-line parameter will greatly increase its verbosity which is\n"
 	printf "  extremely useful when diagnosing issues.\n\n"
 
 	if [ -n "${panic}" ]; then
