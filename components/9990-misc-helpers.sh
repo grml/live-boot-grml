269069291cc11027e3fba8e295d272ec84577604
Raphaël Hertzog <hertzog@debian.org>
2017-01-12 16:31:05 +0100

    Improve removable_dev() in 9990-misc-helpers.sh when /sys/block/ is empty
    Closes: #847802

--- components/9990-misc-helpers.sh
+++ components/9990-misc-helpers.sh
@@ -1169,6 +1169,10 @@ removable_dev ()
 
 	for sysblock in $(echo /sys/block/* | tr ' ' '\n' | grep -vE "/(loop|ram|dm-|fd)")
 	do
+		if [ ! -d "${sysblock}" ]; then
+			continue
+		fi
+
 		dev_ok=
 		if [ "$(cat ${sysblock}/removable)" = "1" ]
 		then
@@ -1214,6 +1218,10 @@ non_removable_dev ()
 
 	for sysblock in $(echo /sys/block/* | tr ' ' '\n' | grep -vE "/(loop|ram|dm-|fd)")
 	do
+		if [ ! -d "${sysblock}" ]; then
+			continue
+		fi
+
 		if [ "$(cat ${sysblock}/removable)" = "0" ]
 		then
 			case "${output_format}" in
