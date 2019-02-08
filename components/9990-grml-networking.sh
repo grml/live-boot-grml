grml/live-boot-grml

commit 52474b4c283033247a3cac42b5551d4380835e7a

9990-grml-networking.sh: display Grml info, update interfaces comment + operate on all net devices
* By displaying "Preconfiguring Grml networking" it's more
  explicit that we're running the 9990-grml-networking.sh code

* It's not the 23networking live-initramfs hook since quite some time,
  update the comment for generation of /etc/network/interfaces

* Support also new style network device names by iterating over
  /sys/class/net/* and skipping only the loopback interface

components/9990-grml-networking.sh
@@ -8,6 +8,8 @@ if [ -n "${NONETWORKING}" ]; then
    return 0
 fi
 
+log_begin_msg "Preconfiguring Grml networking"
+
 modprobe af_packet # req'd for DHCP
 
 # initialize udev
@@ -37,7 +39,7 @@ fi
 
 # config for loopback networking
 cat > $IFFILE << EOF
-# Initially generated on boot by initramfs' 23networking.
+# Initially generated on boot by initramfs
 
 auto lo
 iface lo inet loopback
@@ -47,8 +49,9 @@ EOF
 unset HOSTNAME
 
 # generate config for each present network device
-for interface in /sys/class/net/eth* /sys/class/net/ath* /sys/class/net/wlan*; do
+for interface in /sys/class/net/* ; do
     [ -e ${interface} ] || continue
+    [ "${interface}" = "lo" ] && continue
     interface=$(basename ${interface})
     method="dhcp"
 

commit ad4ec759ed2d8e76de01c9351cfaa6f8062def0c

Do not generate dhcp entry for loopback interface
This fixes a regression that was introduced in commit 52474b4c28303324.
The variable $interface is pointing towards /sys/class/net/lo,
at least until we overwrite the variable by the basename of
its network interface name path. Accordingly fix order for comparison.

Closes: https://github.com/grml/live-boot-grml/issues/9
Thanks: Mykola Malkov for the bug report

components/9990-grml-networking.sh
@@ -51,8 +51,8 @@ unset HOSTNAME
 # generate config for each present network device
 for interface in /sys/class/net/* ; do
     [ -e ${interface} ] || continue
-    [ "${interface}" = "lo" ] && continue
     interface=$(basename ${interface})
+    [ "${interface}" = "lo" ] && continue
     method="dhcp"
 
     # NODHCP or a previously run ipconfig mean that ifupdown should never